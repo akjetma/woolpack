2e08c5368b68c736f5ae9f480ec88806cd846be2
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-01-19 11:32:06 -0800

    clojar prep

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
-(defproject woolpack "0.1.0"
+(defproject akjetma/woolpack "0.1.0"
   :author "Adam Jetmalani <https://akjetma.github.io>"
   :description "Wordcloud layout algorithm"
   :url "http://github.com/akjetma/woolpack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/akjetma/woolpack"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]])
